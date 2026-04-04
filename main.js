622d870b27def813395e78ea9eb43c26b85904bf
olloberdi <ohunpolatov.o@gmail.com>
2026-04-04 15:45:55 +0500

    js masala

---
+++ main.js
@@ -0,0 +1,25 @@
+// рџ§  1. Tezlikni topish
+
+// Masofa s va vaqt t berilgan.
+// Tezlikni toping.
+//Formula  v=s/t
+
+//let s = 100;
+//let t = 2;
+
+//let v = s / t;
+//console.log(v);
+
+//==========================================================================
+// рџ§  2. Masofani topish
+
+// Tezlik v va vaqt t berilgan.
+// Masofani toping.
+
+let v = 60;
+let t = 3;
+let s = 60 * 3;
+console.log(s);
+
+// Begin1. Doiraning radiusi R berilgan. Uning uzunligi L va yuzasi S aniqlansin.
+// L= 2 * ПЂ * R, S = ПЂ * R^2
