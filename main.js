cc337302a625af7cf693efb3524ae41f91a18848
olloberdi <ohunpolatov.o@gmail.com>
2026-04-05 13:26:47 +0500

    birlik va onliklarni ajratdik kn 100nliklar honasidagi sonni topdik

--- main.js
+++ main.js
@@ -33,12 +33,31 @@ console.log("Doira uzunligi (L): " + L);
 console.log("Doira yuzasi (S): " + S);
 //=============================================================================
 // Begin2. Berilgan A va B sonlarining qiymatlarini almashtiring. A va B ning yangi qiymati ekranga chiqarilsin.
-let a = 5;
-let b = 10;
+//let a = 5;
+//let b = 10;
+//let c = a; // 1. A dagi 5 ni C ga olib qo'ydik
+//a = b; // 2. B dagi 10 ni A ga berdik
+//b = c; // 3. C dagi 5 ni B ga berdik
 
-let c = a; // 1. A dagi 5 ni C ga olib qo'ydik
-a = b; // 2. B dagi 10 ni A ga berdik
-b = c; // 3. C dagi 5 ni B ga berdik
+//console.log("A ning yangi qiymati:", a); // 10
+//console.log("B ning yangi qiymati:", b); // 5
+//=============================================================================
+// Ikki xonali son berilgan. Oldin uning o'nliklar xonasidagi raqamini, so`ng birlar xonasidagi raqamini hamda ularning yigвЂ™indisini chiqaruvchi programma tuzilsin.
 
-console.log("A ning yangi qiymati:", a); // 10
-console.log("B ning yangi qiymati:", b); // 5
+//let a = 82;
+
+//let onlik = a / 10;
+
+//let birlik = a % 10;
+
+//console.log("O'nlik:", onlik); // 8
+//console.log("Birlik:", birlik); // 2
+
+//=============================================================================
+// Uch xonali son berilgan. Uning yuzlar xonasidagi raqamini aniqlovchi programma tuzilsin.
+let a = 538; // Ixtiyoriy uch xonali son
+
+// 100 ga bo'lamiz (5.38 chiqadi) va butun qismini olamiz
+let yuzlar = a / 100;
+
+console.log("Yuzlar xonasidagi raqam:", yuzlar); // Natija: 5
